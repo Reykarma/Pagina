2fba591941e2c3b36603fcfdddebed0be6da08ae
Alex Metztli <alexis.luna@alumnos.udg.mx>
2020-02-16 15:54:41 -0600

    Cambios en la estructura de HTML. Seccion contacto, agregada.

--- Server.py
+++ Server.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello():
     return render_template("index.html")
 
+@app.route("/contacto")
+def contacto():
+    return render_template("contacto.html")
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=5001 ,debug=True)
